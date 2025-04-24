3d1599977a34b7e48672c4f9186fe0e18debbb66
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-04-24 10:20:24 +0700

    fix/cors: update configuration to use environment variable

--- VuaTiengViet-API/src/configs/corsConfig.ts
+++ VuaTiengViet-API/src/configs/corsConfig.ts
@@ -1,12 +1,11 @@
+const extendOrigins = process.env.EXTEND_CORS
+  ? process.env.EXTEND_CORS.split(';')
+  : [];
 export const corsConfig = {
   origin: [
-  "http://localhost:5173",
-  "https://localhost:5173",
-  "http://localhost:4200",
-  "http://10.10.21.81:5173",
-  "https://10.10.21.81:5173",
-  "https://dev-mezon.nccsoft.vn",
   "https://vua-tieng-viet.nccsoft.vn",
-  "https://admin.socket.io"],
+  "https://admin.socket.io",
+  ...extendOrigins,
+],
   credentials: true,
 };
